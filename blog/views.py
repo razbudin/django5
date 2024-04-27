6f99367bfc0b7b64824ce7f6acc9bd67b35046f8
razbudin <razbudin@yandex.ru>
2024-04-27 12:17:50 +0300

    Закончена работа с блогом Далее создание блога на классах

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не число, выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number вне диапазона, показать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
