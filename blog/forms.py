df1a4012e5c7b828f9b96988f99ed8c20f06d7da
razbudin <razbudin@yandex.ru>
2024-05-28 23:41:25 +0300

    6.4 Без settings

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
-                               widget=forms.TextArea)
+                               widget=forms.Textarea)
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,38 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [cd['to']])
+            sent = True
+            # ... Отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 # class MyPaginator(Paginator):
